2c3609b01c31d9a31c295235d15c9e1a0f91852d
Erik Lindsay (egl602) <egl602@mail.usask.ca>
2017-01-11 11:35:16 -0600

    Added new Labels and text boxs to Form3
    text boxes don't do anything yet

--- WindowsFormsApplication1/Form3.Designer.cs
+++ WindowsFormsApplication1/Form3.Designer.cs
@@ -40,12 +40,22 @@
             this.EmploymentStatusCombo = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.ErrorLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.EmploymetStatusLabel = new System.Windows.Forms.Label();
+            this.TermDateLabel = new System.Windows.Forms.Label();
+            this.ExtensionNumberLabel = new System.Windows.Forms.Label();
+            this.ExtensionNumberTextBox = new System.Windows.Forms.TextBox();
+            this.JobTitleLabel = new System.Windows.Forms.Label();
+            this.DepartmentLabel = new System.Windows.Forms.Label();
+            this.JobTitleTextBox = new System.Windows.Forms.TextBox();
+            this.DepartmentTextBox = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // NextButton
             // 
-            this.NextButton.Location = new System.Drawing.Point(228, 171);
+            this.NextButton.Location = new System.Drawing.Point(111, 0);
             this.NextButton.Name = "NextButton";
             this.NextButton.Size = new System.Drawing.Size(75, 23);
             this.NextButton.TabIndex = 0;
@@ -55,7 +65,7 @@
             // 
             // BackButton
             // 
-            this.BackButton.Location = new System.Drawing.Point(118, 171);
+            this.BackButton.Location = new System.Drawing.Point(1, 0);
             this.BackButton.Name = "BackButton";
             this.BackButton.Size = new System.Drawing.Size(75, 23);
             this.BackButton.TabIndex = 2;
@@ -133,6 +143,10 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.ExtensionNumberTextBox);
+            this.panel1.Controls.Add(this.ExtensionNumberLabel);
+            this.panel1.Controls.Add(this.TermDateLabel);
+            this.panel1.Controls.Add(this.EmploymetStatusLabel);
             this.panel1.Controls.Add(this.EmploymentStatusCombo);
             this.panel1.Controls.Add(this.TermDate);
             this.panel1.Controls.Add(this.MiddleInitialTextBox);
@@ -143,7 +157,7 @@
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(406, 124);
+            this.panel1.Size = new System.Drawing.Size(406, 201);
             this.panel1.TabIndex = 11;
             // 
             // ErrorLabel
@@ -155,17 +169,96 @@
             this.ErrorLabel.Size = new System.Drawing.Size(0, 13);
             this.ErrorLabel.TabIndex = 12;
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.BackButton);
+            this.panel2.Controls.Add(this.NextButton);
+            this.panel2.Location = new System.Drawing.Point(115, 241);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(188, 29);
+            this.panel2.TabIndex = 13;
+            // 
+            // EmploymetStatusLabel
+            // 
+            this.EmploymetStatusLabel.AutoSize = true;
+            this.EmploymetStatusLabel.Location = new System.Drawing.Point(13, 82);
+            this.EmploymetStatusLabel.Name = "EmploymetStatusLabel";
+            this.EmploymetStatusLabel.Size = new System.Drawing.Size(94, 13);
+            this.EmploymetStatusLabel.TabIndex = 11;
+            this.EmploymetStatusLabel.Text = "EmploymentStatus";
+            // 
+            // TermDateLabel
+            // 
+            this.TermDateLabel.AutoSize = true;
+            this.TermDateLabel.Location = new System.Drawing.Point(143, 82);
+            this.TermDateLabel.Name = "TermDateLabel";
+            this.TermDateLabel.Size = new System.Drawing.Size(122, 13);
+            this.TermDateLabel.TabIndex = 12;
+            this.TermDateLabel.Text = "Account Expiration Date";
+            // 
+            // ExtensionNumberLabel
+            // 
+            this.ExtensionNumberLabel.AutoSize = true;
+            this.ExtensionNumberLabel.Location = new System.Drawing.Point(293, 81);
+            this.ExtensionNumberLabel.Name = "ExtensionNumberLabel";
+            this.ExtensionNumberLabel.Size = new System.Drawing.Size(35, 13);
+            this.ExtensionNumberLabel.TabIndex = 13;
+            this.ExtensionNumberLabel.Text = "Ext #:";
+            // 
+            // ExtensionNumberTextBox
+            // 
+            this.ExtensionNumberTextBox.Location = new System.Drawing.Point(296, 98);
+            this.ExtensionNumberTextBox.Name = "ExtensionNumberTextBox";
+            this.ExtensionNumberTextBox.Size = new System.Drawing.Size(65, 20);
+            this.ExtensionNumberTextBox.TabIndex = 14;
+            // 
+            // JobTitleLabel
+            // 
+            this.JobTitleLabel.AutoSize = true;
+            this.JobTitleLabel.Location = new System.Drawing.Point(36, 167);
+            this.JobTitleLabel.Name = "JobTitleLabel";
+            this.JobTitleLabel.Size = new System.Drawing.Size(50, 13);
+            this.JobTitleLabel.TabIndex = 14;
+            this.JobTitleLabel.Text = "Job Title:";
+            // 
+            // DepartmentLabel
+            // 
+            this.DepartmentLabel.AutoSize = true;
+            this.DepartmentLabel.Location = new System.Drawing.Point(152, 167);
+            this.DepartmentLabel.Name = "DepartmentLabel";
+            this.DepartmentLabel.Size = new System.Drawing.Size(65, 13);
+            this.DepartmentLabel.TabIndex = 15;
+            this.DepartmentLabel.Text = "Department:";
+            // 
+            // JobTitleTextBox
+            // 
+            this.JobTitleTextBox.Location = new System.Drawing.Point(39, 184);
+            this.JobTitleTextBox.Name = "JobTitleTextBox";
+            this.JobTitleTextBox.Size = new System.Drawing.Size(100, 20);
+            this.JobTitleTextBox.TabIndex = 16;
+            // 
+            // DepartmentTextBox
+            // 
+            this.DepartmentTextBox.Location = new System.Drawing.Point(155, 183);
+            this.DepartmentTextBox.Name = "DepartmentTextBox";
+            this.DepartmentTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DepartmentTextBox.TabIndex = 17;
+            // 
             // Form3
             // 
-            this.ClientSize = new System.Drawing.Size(416, 206);
+            this.ClientSize = new System.Drawing.Size(416, 300);
+            this.Controls.Add(this.DepartmentTextBox);
+            this.Controls.Add(this.JobTitleTextBox);
+            this.Controls.Add(this.DepartmentLabel);
+            this.Controls.Add(this.JobTitleLabel);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.ErrorLabel);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.BackButton);
-            this.Controls.Add(this.NextButton);
             this.Name = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load_1);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -184,5 +277,14 @@
         private System.Windows.Forms.ComboBox EmploymentStatusCombo;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label ErrorLabel;
+        private System.Windows.Forms.TextBox ExtensionNumberTextBox;
+        private System.Windows.Forms.Label ExtensionNumberLabel;
+        private System.Windows.Forms.Label TermDateLabel;
+        private System.Windows.Forms.Label EmploymetStatusLabel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label JobTitleLabel;
+        private System.Windows.Forms.Label DepartmentLabel;
+        private System.Windows.Forms.TextBox JobTitleTextBox;
+        private System.Windows.Forms.TextBox DepartmentTextBox;
     }
 }
